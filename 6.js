39a4e66a68b015d1f91d868f42427662768ad10f
Abraham <a355208@uach.mx>
2024-04-30 12:41:33 -0600

    Problema 6: Hacer un programa que imprima todos los numeros naturales que hay desde el uno hasta un numero que introducimos por el teclado

---
+++ 6.js
@@ -0,0 +1,28 @@
+/* Hacer un programa que imprima todos los números naturales que hay desde el uno hasta un
+número que introducimos por teclado.
+Esto fue buscado en internet para encontrar como leer el teclado 
+Enlace: https://es.stackoverflow.com/questions/445091/esperar-respuesta-de-readline-en-un-loop-en-javascript*/
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Introduce un número límite: ", function(numeroUsuario) {
+  const numero = parseInt(numeroUsuario);
+  let ini = 1
+  do{
+    if (ini <= numero) {
+      console.log(ini);
+      ini += 1
+    }
+  }
+  while (numero > 0 && ini <= numero) 
+  
+  if(numero < 0){
+    console.log("No se permiten numeros negativos");
+  }
+
+  rl.close();
+});
